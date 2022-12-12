18b104025ce8b91786d36d025d305b7e496d16d9
Füredi Dániel <daniel.furedi03@gmail.com>
2022-12-12 17:15:11 +0100

    nest.js structure set up docker, prisma + models set up sign up auth set up

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+
+@Injectable()
+export class AuthService{
+
+    constructor(private prisma: PrismaService){}
+   
+    async login(dto: AuthDto) {
+        
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            };
+        });
+
+        return {msg: 'I have logged in!'}
+    }
+
+    async signUp(dto: AuthDto){
+        const hash = await argon.hash(dto.password)
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+            delete user.hash;
+    
+            return user;
+        } catch (error){
+            if (error instanceof PrismaClientKnownRequestError){
+                if (error.code === 'P2002'){
+                    throw new ForbiddenException('Credentials already taken')
+                }
+            }
+            throw error;
+        }
+        
+    }
+}
